a4d59ccb9df00bd57dff3f46cf0a76333ff3c2c5
samueloliveiraf <samueldiablo73@gmail.com>
2021-08-19 19:18:55 -0300

    add

--- store/models.py
+++ store/models.py
@@ -15,7 +15,6 @@ class Establishment(models.Model):
 	establishment = models.BooleanField(default=False)
 
 
-
 class Customer(models.Model):
 	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)

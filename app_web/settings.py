9347c34f56072eaaa832e5e188e0f6ff41fed152
samueloliveiraf <samueldiablo73@gmail.com>
2021-08-19 20:56:31 -0300

    add ok

--- app_web/settings.py
+++ app_web/settings.py
@@ -116,11 +116,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 

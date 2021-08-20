5e8d757390a4764ed76d77703a1ccbcd8344759c
samueloliveiraf <samueldiablo73@gmail.com>
2021-08-20 18:08:27 -0300

    ok

--- app_web/settings.py
+++ app_web/settings.py
@@ -125,6 +125,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

26c6a6eaac582da4ff5ed7725421dcf768fae2d5
samueloliveiraf <samueldiablo73@gmail.com>
2021-08-19 21:30:25 -0300

    add ok

--- app_web/settings.py
+++ app_web/settings.py
@@ -114,9 +114,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

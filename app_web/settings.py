5397e6effa5ee539598f99ac4a5b781c9308f879
samueloliveiraf <samueldiablo73@gmail.com>
2021-08-20 09:26:39 -0300

    ok

--- app_web/settings.py
+++ app_web/settings.py
@@ -119,12 +119,14 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'setup/static'),
+    os.path.join(BASE_DIR,'static'),
 ]
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 MEDIA_URL='/media/'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

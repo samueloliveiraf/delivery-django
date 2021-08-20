914b55ea61840c4ea8a70af2f29585d40f544c57
samueloliveiraf <samueldiablo73@gmail.com>
2021-08-20 18:13:54 -0300

    ok

--- app_web/settings.py
+++ app_web/settings.py
@@ -117,12 +117,13 @@ USE_TZ = True
 STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "staticfiles",
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
 MEDIA_URL='/media/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

45c0490b5fcc5b60f22a1c6ad3136b265318922e
samueloliveiraf <samueldiablo73@gmail.com>
2021-08-19 21:52:28 -0300

    add ok

--- app_web/settings.py
+++ app_web/settings.py
@@ -123,8 +123,6 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

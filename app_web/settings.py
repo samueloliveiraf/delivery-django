0e444302341965b96e0f542fe27a1fa9b9821664
samueloliveiraf <samueldiablo73@gmail.com>
2021-08-31 18:51:52 -0300

    ok

--- app_web/settings.py
+++ app_web/settings.py
@@ -124,7 +124,6 @@ MEDIA_URL='/media/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

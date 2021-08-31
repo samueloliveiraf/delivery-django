e66ce126104780d2e07b5c1ebcc0800a3130d5f7
samueloliveiraf <samueldiablo73@gmail.com>
2021-08-31 19:14:03 -0300

    ok

--- app_web/settings.py
+++ app_web/settings.py
@@ -119,7 +119,7 @@ STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
